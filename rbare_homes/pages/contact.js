f0e25cd23ba0afe3cd2821550c7bbb6184a8bbf8
aj <ajnarag25@gmail.com>
2022-12-06 05:23:14 +0800

    created homepage and contact page

---
+++ rbare_homes/pages/contact.js
@@ -0,0 +1,132 @@
+// add bootstrap css 
+import 'bootstrap/dist/css/bootstrap.css'
+import styles from '../styles/custom.module.css'
+
+
+function index(){
+    return <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+        <div className="container">
+            <a className={styles.custom_brand} href="#">RBare Homes Co.</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <ul class="navbar-nav ">
+                <li class="">
+                    <a className="nav-link" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a className="nav-link active" href="#">Contact</a>
+                </li>
+                    <li class="nav-item">
+                    <a className="nav-link" href="#">About  </a>
+                </li>			
+            </ul>		  
+            </div>
+        </div>
+    </nav>
+ 
+    <div className={styles.hero_contact}>
+        <div className={styles.contact_right}>
+        <h3 className={styles.titles}>Contact</h3>
+        <br/>
+        <p className={styles.ptag}>Contact us and we'll get back to you within 24 hours.</p>
+        </div>
+    </div>
+
+
+
+
+
+    <div className="mt-4 p-5 text-center">
+        <div className="row">
+            
+            <div className="col-md-4">
+                <div className={styles.card_custom}>
+                    <img className={styles.logo} src="./contact.jpg" width="500" alt="" />
+                </div>
+            </div>
+            <br/>
+            <div className="col-md-8" id="readmore">
+                <div className={styles.card_content}>
+                <h4 className={styles.text_card}>Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within
+                    a matter of hours to help you.</h4>
+                 <br/>
+                 <hr/>
+                 <div class="row">
+                        <div class="col-md-9 mb-md-0 mb-5">
+                            <form id="contact-form" name="contact-form" action="mail.php" method="POST">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="md-form mb-0">
+                                            <input type="text" id="name" name="name" placeholder="Enter Name" class="form-control" required/>
+                                        </div>
+                                        <br/>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="md-form mb-0">
+                                            <input type="text" id="email" name="email" placeholder="Enter Email" class="form-control" required/>
+                                        </div>
+                                        <br/>
+                                    </div>
+
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="md-form mb-0">
+                                            <input type="text" id="subject" name="subject" placeholder="Subject" class="form-control" required/>
+                                        </div>
+                                        <br/>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="md-form">
+                                            <textarea type="text" id="message" name="message" rows="5" placeholder="Message" class="form-control md-textarea" required></textarea>
+                                        </div>
+                                        <br/>
+                                    </div>
+                                </div>
+
+                                <button className="btn btn-primary w-100">Send</button>
+                            </form>
+                            <div class="status"></div>
+                        </div>
+                        <div class="col-md-3 text-center">
+                            <ul class="list-unstyled mb-0">
+                                <li><box-icon name='map'></box-icon>
+                                    <p>Main Office Monterra Homes, Imus</p>
+                                </li>
+
+                                <li><box-icon name='phone' ></box-icon>
+                                    <p>(046) 437-7738, 09568326116</p>
+                                </li>
+
+                                <li><box-icon name='envelope' ></box-icon>
+                                    <p>rbare_homes@gmail.com</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <footer class="text-center text-white mt-4 p-5">    
+        <div class="text-center text-dark p-3">
+            © 2022 All Rights Reserved:
+        <a className={styles.footer_link} href="#"> Rbare Homes Co.</a>
+        </div>
+    </footer>
+  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+    </div>
+
+
+
+}
+export default index;
--- rbare_homes/pages/index.js
+++ rbare_homes/pages/index.js
@@ -5,7 +5,7 @@ import styles from '../styles/custom.module.css'
 
 function index(){
     return <div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-success">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
         <div className="container">
             <a className={styles.custom_brand} href="#">RBare Homes Co.</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -14,30 +14,64 @@ function index(){
             <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
             <ul class="navbar-nav ">
                 <li class="">
-                    <a className={styles.custom_links} href="#">Home</a>
+                    <a className="nav-link active" href="#">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a className={styles.custom_links} href="#">Contact</a>
+                    <a className="nav-link" href="contact">Contact</a>
                 </li>
                     <li class="nav-item">
-                    <a className={styles.custom_links} href="#">About  </a>
+                    <a className="nav-link" href="#">About  </a>
                 </li>			
             </ul>		  
             </div>
         </div>
     </nav>
  
-    <div>
-        <img className="d-block w-100" src="house.png" alt="" />
-        <h3 className={styles.textleft} >Dream it, <br/> Claim it.</h3>
-    </div>
-
-    <div className={styles.custom_jumbotron}>
-        <div>
-            
+    <div className={styles.hero_bg}>
+        <div className={styles.textleft}>
+        <h3 className={styles.titles}>Dream it, <br/> Claim it.</h3>
+        <br/>
+        <p className={styles.ptag}>Experience the best real-estate service.</p>
+        <br/>
+        <form action="#readmore">
+        <button type="submit" className={styles.custom_button}>Read More</button>   
+        </form>
         </div>
     </div>
 
+    <div className="mt-4 p-5 text-center">
+        <div className="row">
+            
+            <div className="col-md-4">
+                <div className={styles.card_custom}>
+                    <img className={styles.logo} src="./side_house.jpg" width="500" alt="" />
+                </div>
+            </div>
+            <br/>
+            <div className="col-md-8" id="readmore">
+                <div className={styles.card_content}>
+                <h1 className={styles.text_card}>RBare Homes Affordable House <br/> and Lot in Sta. Cruz, Laguna</h1>
+                 <br/>
+                 <hr/>
+                 <p className={styles.p_card}>Known as the Home of Kesong Puti, Sta. Cruz city is an urbanized town at th
+                 e heart of Laguna. The town has been the center of commercial businesses in the eastern part of the province.
+                 As a result, the town boasts of recreational tourist spots and a plaza, <br/> wherein jeepneys from different locations in
+                 Laguna can be easily accessed.
+                </p>
+                </div>
+               
+            </div>
+        </div>
+    </div>
+
+
+    <footer class="text-center text-white mt-4 p-5">
+
+        <div class="text-center text-dark p-3">
+            © 2022 All Rights Reserved:
+        <a className={styles.footer_link} href="#"> Rbare Homes Co.</a>
+        </div>
+    </footer>
   
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
