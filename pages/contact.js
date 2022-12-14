505e7d5e4c367098aefb0159c8f44e6afca2eedd
SeanDotExe <s.monacillo59@gmail.com>
2022-12-15 02:37:17 +0800

    changes

--- pages/contact.js
+++ pages/contact.js
@@ -2,6 +2,7 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import styles from '../styles/custom.module.css'
 import Link from 'next/link';
+import Script from 'next/script'
 
 function index(){
     
@@ -123,8 +124,8 @@ function index(){
         </div>
     </footer>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+    <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></Script>
+    <Script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></Script>
     </div>
 
 
